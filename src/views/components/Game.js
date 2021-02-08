bfce48490f9c6419b5fc2cc03ee26499f7c11d92
9bini <gotn0727@gmail.com>
2021-02-08 10:37:40 +0900

    ~ 시간 여행 구현하기

--- src/views/components/Game.js
+++ src/views/components/Game.js
@@ -7,12 +7,21 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            stepNumber: 0,
             xIsNext: true
         };
     }
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            // stepNumber가 짝수일 때 마다 xIsNext를 true로 설정
+            xIsNext: (step % 2) === 0,
+        });
+    }
 
     handleClick(i) {
-        const history = this.state.history;
+        // 클릭할 때 마다 히스토리를 잘라 미래 기록을 날림
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -23,13 +32,14 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
-    }
+  }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
@@ -37,7 +47,7 @@ class Game extends React.Component {
                 'Go to move #' + move :
                 'Go to game start';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
